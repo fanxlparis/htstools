abe02431b6e617abb9d181d05e5f1aeae8a2711c
Jan Voges <janvoges@me.com>
2018-09-19 16:38:18 -0500

    Cleanup

--- source/apply_bqsr.sh
+++ source/apply_bqsr.sh
@@ -1,40 +1,75 @@
 #/bin/bash
 
-script_name="$(basename $0)"
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
+
+readonly SCRIPT_NAME="$(basename $0)"
 
 
 # -----------------------------------------------------------------------------
-# input files
+# Functions
 # -----------------------------------------------------------------------------
 
-reference_fasta=""
-input_bam=""
-dbsnp_vcf=""
+safe_execute ()
+{
+    local readonly command=("$@")
+    "${command[@]}"
+    # "${command[@]}" &> /dev/null
+    local readonly return_code=$?
+    if [ $return_code -ne 0 ]; then
+        cmd="${command[@]}"
+        printf "$SCRIPT_NAME: error: '$cmd' returned with non-zero status\n"
+        exit -1
+    fi
+}
+
+
+# -----------------------------------------------------------------------------
+# Parse command line.
+# -----------------------------------------------------------------------------
+
+if [ "$#" -ne 0 ]; then
+    printf "$SCRIPT_NAME: usage: $0\n";
+    exit -1;
+fi
+
+
+# -----------------------------------------------------------------------------
+# Input files
+# -----------------------------------------------------------------------------
+
+readonly reference_fasta=""
+readonly input_bam=""
+readonly dbsnp_vcf=""
 
 if [ ! -f $reference_fasta ]; then
-    printf "$script_name: error: '$reference_fasta' is not a regular file\n"
+    printf "$SCRIPT_NAME: error: '$reference_fasta' is not a regular file\n"
     exit -1
 fi
 
 if [ ! -f $input_bam ]; then
-    printf "$script_name: error: '$input_bam' is not a regular file\n"
+    printf "$SCRIPT_NAME: error: '$input_bam' is not a regular file\n"
     exit -1
 fi
 
 if [ ! -f $dbsnp_vcf ]; then
-    printf "$script_name: error: '$dbsnp_vcf' is not a regular file\n"
+    printf "$SCRIPT_NAME: error: '$dbsnp_vcf' is not a regular file\n"
     exit -1
 fi
 
 
 # -----------------------------------------------------------------------------
-# executables
+# Check if required executables are available.
 # -----------------------------------------------------------------------------
 
-gatk="/project/omics/bin/gatk-4.0"
+# !!!!!!!!!!!!!!! YOUR CUSTOM PATHS MIGHT BE REQUIRED HERE !!!!!!!!!!!!!!!!!!!!
+readonly gatk="/project/omics/bin/gatk-4.0"
+# !!!!!!!!!!!!!!! YOUR CUSTOM PATHS MIGHT BE REQUIRED HERE !!!!!!!!!!!!!!!!!!!!
 
 if [ ! -x $gatk ]; then
-    printf "$script_name: binary file '$gatk' is not executable\n"
+    printf "$SCRIPT_NAME: binary file '$gatk' is not executable\n"
     exit -1
 fi
 
